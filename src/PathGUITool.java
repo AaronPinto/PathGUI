6bf2786c02179e32ab978056045dec5626ffa8c7
Aaron Pinto <AaronPinto@users.noreply.github.com>
2019-02-17 04:17:46 -0500

    Update PathGUITool.java

--- src/PathGUITool.java
+++ src/PathGUITool.java
@@ -13,7 +13,7 @@ import java.util.stream.*;
 /**
  * The program takes the mouse position on the field drawn in the GUI and then based off of that, when the mouse button is
  * clicked, it prompts the user for a desired heading, velocity and acceleration for that point and then adds that point
- * to a series of points and generates a path accordingly. The path generation is based of cubic spline interpolation.
+ * to a series of points and generates a path accordingly. The path generation is based of quintic spline interpolation.
  *
  * @author Aaron Pinto
  * @see BetterArrayList
